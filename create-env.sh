7add71dbfafac79467242436e49d7ae033920050
Ubuntu <ubuntu@ip-172-31-16-255.ap-south-1.compute.internal>
2019-03-30 13:16:59 +0000

    changed o in image name

--- create-env.sh
+++ create-env.sh
@@ -12,7 +12,7 @@ do
 	echo "%%%%%%%%%%%%%%%%%%%&&"
 	echo "creating container $i ...."
 	sleep 1
-docker run --name project$i -d -it --rm singamndevops/project /bin/bash
+docker run --name project$i -d -it --rm singamndevops/prject /bin/bash
 
 echo "containername $i has been created#"
 
