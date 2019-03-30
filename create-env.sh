dbd52cd73f0a40ba912ca59eddfff81815cb2287
Ubuntu <ubuntu@ip-172-31-31-43.ap-south-1.compute.internal>
2019-03-30 11:59:33 +0000

    added script to create containers

---
+++ create-env.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+cont_count=$1
+
+echo "creating $cont_count containers......"
+
+sleep 2;
+
+for i in 'seq $cont_count'
+
+do
+	echo "%%%%%%%%%%%%%%%%%%%"
+	echo "creating container $i ...."
+	sleep 1
+docker run --name project$i -d -it --rm singamndevops/project /bin/bash
+
+echo "containername $i has been created#"
+
+
+echo "%%%%%%%%%%%%%%%%%%%%%%%%"
+
+done
+
